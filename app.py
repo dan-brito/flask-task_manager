b6c63a0d68366e3a8d0b5ad4095b213032770080
Danilo-Brito-Rocha <danilobrittorocha@gmail.com>
2023-02-04 16:31:43 -0300

    Extending HTML Templates

--- app.py
+++ app.py
@@ -5,7 +5,11 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('index.html', content='Content for Home Page')
+    return render_template('index.html', content = 'Home Page')
+
+@app.route('/about')
+def about():
+    return render_template('about.html', content = 'About Page')
 
 if __name__ == '__main__':
     app.run(debug=True)
