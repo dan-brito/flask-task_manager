75cba6a9472a579a60ffaa5c0f8c214693046b52
Danilo-Brito-Rocha <danilobrittorocha@gmail.com>
2023-02-12 20:59:08 -0300

    SQLAlchemy

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'mysecretkey'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+
+app.app_context().push()
+
+db = SQLAlchemy(app)
 
 from routes import *
 
---
+++ models.py
@@ -0,0 +1,10 @@
+from app import db
+
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    date = db.Column(db.Date, nullable=False)
+
+    def __repr__(self):
+        return '{.title} created on {.date}'.format(self)
+
