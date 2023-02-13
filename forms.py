05de9943cf9347785bba3992244f5fc6cc651f3d
Danilo-Brito-Rocha <danilobrittorocha@gmail.com>
2023-02-12 21:10:22 -0300

    Updating and Using the Database

--- forms.py
+++ forms.py
@@ -2,6 +2,7 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 
-class Form(FlaskForm):
+class Task(FlaskForm):
     title = StringField('Title', validators=[DataRequired()])
     submit = SubmitField('Submit')
+    date = StringField('Date')
--- routes.py
+++ routes.py
@@ -1,19 +1,24 @@
-from app import app
-from flask import render_template
+from app import app, db
+from flask import render_template, redirect, url_for
+from models import Task
+from datetime import datetime
 
 import forms
 
 @app.route('/')
-@app.route('/index')
-def index():
-    return render_template('index.html', content = 'Home Page')
+@app.route('/task')
+def task():
+    tasks = Task.query.all() 
+    return render_template('task.html', content = 'Tasks', tasks=tasks)
 
-@app.route('/about', methods=['GET', 'POST'])
-def about():
-    form = forms.Form()
+@app.route('/add', methods=['GET', 'POST'])
+def add():
+    form = forms.Task()
     if form.validate_on_submit():
-        print("Form Submitted:", form.title.data)
-        return render_template('about.html', content = 'About Page', form=form, title=form.title.data)
-    return render_template('about.html', content = 'About Page', form=form)
+        task = Task(title=form.title.data, date=datetime.utcnow())
+        db.session.add(task)
+        db.session.commit()
+        return redirect(url_for('task'))
+    return render_template('add.html', content = 'Add new', form=form)
 
 
